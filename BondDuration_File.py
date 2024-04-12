ae118c9a8017ce0f4267eb978ccc7a94772a1f77
yy3335 <165346669+yy3335@users.noreply.github.com>
2024-04-11 22:00:54 -0400

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,3 +1,15 @@
 
 def getBondDuration(y, face, couponRate, m, ppy = 1):
-    return(8.51)
+    pmt = face * couponRate / ppy
+    duration = 0
+    pvsum = 0  
+    for t in range(1, m*ppy  + 1):
+        pv = pmt / ((1 + y/ppy) ** t)
+        if t == m * ppy:
+            pv += face / ((1 + y/ppy) ** t) 
+        duration += t * pv
+        pvsum += pv
+
+    duration /= pvsum  
+    duration /= ppy     
+    return duration
