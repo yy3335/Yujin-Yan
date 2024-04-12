8f9926c880e0c2285ef38cb83d410751d53e4902
yy3335 <165346669+yy3335@users.noreply.github.com>
2024-04-11 22:03:03 -0400

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,5 +1,12 @@
-
-
 def FizzBuzz(start, finish):
-    v = ['buzz', 41, 'fizz', 43, 44, 'fizzbuzz']
-    return(v)
+    outlist = []
+    for i in range(start, finish + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            outlist.append("fizzbuzz")
+        elif i % 3 == 0:
+            outlist.append("fizz")
+        elif i % 5 == 0:
+            outlist.append("buzz")
+        else:
+            outlist.append(i)
+    return outlist
