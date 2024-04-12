e139c4b2ffe05e7cbf4ae82f2f45159f58f549f6
yy3335 <165346669+yy3335@users.noreply.github.com>
2024-04-11 22:01:13 -0400

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,4 +1,10 @@
-
-
 def getBondPrice_E(face, couponRate, m, yc):
-    return(2098949)
+    pmt = face * couponRate
+    bondPrice = 0
+    for t, ytm in enumerate(yc, start=1):
+        pv = pmt / ((1 + ytm) ** t)
+        bondPrice += pv
+        if t == m:
+            pvcf = face / ((1 + ytm) ** t)
+            bondPrice += pvcf
+    return bondPrice
